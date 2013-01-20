35a889e39f58333babbbb15f5998540f3efd66cd
iceafish <iceafish@gmail.com>
2013-01-20 14:53:54 +0800

    Signed-off-by: iceafish <iceafish@gmail.com>

--- OnlineJudge/OnlineJudge/urls.py
+++ OnlineJudge/OnlineJudge/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls import patterns, include, url
 from views import index
-
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', index),
     
-
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
