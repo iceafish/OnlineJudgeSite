420d709a559cb7fcafe17d8fd3b5ec985479e90d
kitty <cottonyin2012@gmail.com>
2013-01-21 11:04:29 +0800

    Change-Id: Ifcfac6bce80ca51751e48874789b2b9c718f06dd

--- OnlineJudge/views.py
+++ OnlineJudge/views.py
@@ -2,9 +2,11 @@
 from django.shortcuts import render_to_response
 
 def show_index( request ):
-    return render_to_response("index.html", { 'bodyType':'home' })
+    
+    return render_to_response("index.html", locals() )
 
 # Home Contests Problems Status Ranklist F.A.Qs
 
 def show_faq( request ):
-    return render_to_response("index.html", { 'bodyType': 'faq' })
+    
+    return render_to_response("faq.html", locals() )
