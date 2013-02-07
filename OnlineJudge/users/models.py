de45daecbe2b27f8180573f92c40c734890d493b
kitty <cottonyin2012@gmail.com>
2013-02-07 19:22:01 +0800

    Signed-off-by: kitty <cottonyin2012@gmail.com>

--- OnlineJudge/users/models.py
+++ OnlineJudge/users/models.py
@@ -1,16 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 class UserModel(models.Model):
-    user = models.OneToOneField(User)
-    gender = models.BooleanField(blank = True)
-    school = models.CharField(max_length=128, blank=True)
+    #user = models.OneToOneField(User)
+    username = models.CharField(max_length=20,unique=True)
+    password = models.CharField(max_length=20)
+    
+    
+    '''gender = models.BooleanField(blank = True)'''
+    '''school = models.CharField(max_length=128, blank=True)'''
+    
+    
     '''
     school can set manytomany type, usr can choose one of these.
     if databases not exist, use can create a new one.
     but now just design a base system.
     '''
-    info = models.TextField(blank=True)
+    '''info = models.TextField(blank=True)'''
 
     def __unicode__(self):
         return u'%s' % (unicode(self.user.username))
