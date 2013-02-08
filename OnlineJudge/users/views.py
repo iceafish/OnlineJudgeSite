5416bd67aba17be8fa397f5e32efd1e5d51d8219
kitty <cottonyin2012@gmail.com>
2013-02-08 19:49:56 +0800

    Signed-off-by: kitty <cottonyin2012@gmail.com>

--- OnlineJudge/users/views.py
+++ OnlineJudge/users/views.py
@@ -5,10 +5,22 @@ from django.template import RequestContext
 
 from users.models import UserModel
 def login(request):
-    
-    
-    return HttpResponse("loggin have no this problem.")
-
+    if request.method == 'POST':
+        if username not in request.POST or password not in request.POST:
+            raise  Http404('Please input username and password')
+            if user_id not in request.session:
+                try:
+                    u = UserModel.objects.get(username = username)
+                except:
+                    raise Http404('Not exist this username')
+                if u.password == request.POST['password'] : 
+                    request.session['user_id'] = u.id
+                 
+        else:
+            raise Http404("You are logged in.")
+        
+        return render_to_response("user/success.html",context_instance=RequestContext(request))
+    raise Http404("Only post allow")
 
 def reg(request):
     if request.method == 'POST':
