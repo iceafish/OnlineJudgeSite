083cd308cbdf4b02e0ee322f594305dd505290ce
kitty <cottonyin2012@gmail.com>
2013-02-08 17:33:11 +0800

    Signed-off-by: kitty <cottonyin2012@gmail.com>

--- OnlineJudge/users/views.py
+++ OnlineJudge/users/views.py
@@ -1,10 +1,18 @@
-
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 def login(request):
     return HttpResponse("loggin have no this problem.")
     
 def reg(request):
-    
-    return render_to_response("users/reg.html")
+    if request.method == 'POST':
+        if 'username' not in request.POST or 'password' not in request.POST:
+            raise Http404('username or password not in POST')
+        
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        
+    else:
+        return render_to_response("users/reg.html")
     
