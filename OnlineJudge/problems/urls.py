956ef05e63f20be200780480eed50eb49b09c437
iceafish <iceafish@iceafish-VAIO>
2013-02-08 11:38:20 +0800

    Signed-off-by: iceafish <iceafish@iceafish-VAIO>

--- OnlineJudge/problems/urls.py
+++ OnlineJudge/problems/urls.py
@@ -4,10 +4,4 @@ from problems.views import show_problem_list,show_problem
 urlpatterns = patterns('', 
     url(r'^$',show_problem_list),
     url(r'^(\d+)$', show_problem),
-    
-    
-    #url(r'^problemlist/(\d+)/$',show_problem_list),
-    #url(r'^problem/(\d+)/$',show_problem),
-    
-    
 )
