bac4fff839b8396010506aeca68068c49de10c9f
iceafish <iceafish@iceafish-VAIO>
2013-02-08 10:58:23 +0800

    Signed-off-by: iceafish <iceafish@iceafish-VAIO>

--- OnlineJudge/problems/urls.py
+++ OnlineJudge/problems/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
-from problems.views import show_problem_list 
+from problems.views import show_problem_list,show_problem
 
 urlpatterns = patterns('', 
     url(r'^$',show_problem_list),
-    url(r'^(\d+)$', show_problem_list),
+    url(r'^(\d+)$', show_problem),
     
     
     #url(r'^problemlist/(\d+)/$',show_problem_list),
--- OnlineJudge/problems/views.py
+++ OnlineJudge/problems/views.py
@@ -16,13 +16,8 @@ def show_problem_list( request, page = 1 ):
     end = page*100+1
     '''
     problems = models.Problem.objects.filter()
-    '''
-    #print problems
-    for x in problems:
-        print  "%s | %s"  %  (x.id,x.title)
-    '''
     
     return render_to_response( "problemlist.html", { 'problems': problems } )
 
-def show_problem(request):
-    return render_to_response("problem.html",locals() )
+def show_problem( request, problem_id ):
+    return render_to_response("problemInfo.html",locals() )
