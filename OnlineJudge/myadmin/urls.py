9e032aed6cee70a2cacabcd6291cd5703c10fd8f
kitty <cottonyin2012@gmail.com>
2013-02-11 15:54:31 +0800

    Signed-off-by: kitty <cottonyin2012@gmail.com>

--- OnlineJudge/myadmin/urls.py
+++ OnlineJudge/myadmin/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('', 
     url(r'^$', show_manage ),
+    url(r'^logout/$',logout),
     url(r'^index/$',show_index),
     url(r'^login/$',login),
 )
--- OnlineJudge/myadmin/views.py
+++ OnlineJudge/myadmin/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render_to_response
 from django.http import Http404
 from django.http import HttpResponseRedirect
 from myadmin.models import AdminUser
+from django.template import RequestContext
 # Create your views here.
 
 # myAdmin
 def show_manage( request ):
-    return render_to_response("myadmin/login.html")
+    if "admin_username" not in request.session:
+        #return render_to_response("myadmin/login.html",context_instance=RequestContext(request))
+        return HttpResponseRedirect("/myadmin/login/")
+    else:
+        return HttpResponseRedirect("/myadmin/index/")
 def login(request):
     if "admin_username" in request.session:
         # is already login
@@ -16,22 +21,23 @@ def login(request):
         if "username" not in request.POST or "password" not in request.POST:
             raise Http404("Please input username and password!")
         try:
-            u=AdminUser.objects.get(username=username,password=password)
+            u=AdminUser.objects.get(username=request.POST['username'],password=request.POST['password'])
         except AdminUser.DoesNotExist:
             raise Http404("Login Failed!")
-        request.session["admin_uesrname"] = u.username
+        request.session["admin_username"] = u.username
         return HttpResponseRedirect("/myadmin/index/")
     else:
-        return HttpResponseRedirect("/myadmin/")
-    
+        return render_to_response("myadmin/login.html",context_instance=RequestContext(request))
+def logout(request):
+    if "admin_username" in request.session:
+        del request.session['admin_username']
+    return HttpResponseRedirect('/myadmin/')       
+
 def show_index(request):
     if "admin_username" not in request.session:
         #is not login
         return HttpResponseRedirect("/myadmin/")
-    #
-    #
-    #
-    #
-    #
-    #
+    
+    
+    
     return render_to_response("myadmin/index.html",locals())
